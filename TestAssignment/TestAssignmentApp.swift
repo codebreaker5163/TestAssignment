b68344ea258f0960131a1a7d5a8525c02e6f83e7
codebreaker5163 <33778814+codebreaker5163@users.noreply.github.com>
2025-08-15 09:43:40 +0530

    Initial Commit

---
+++ TestAssignment/TestAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAssignmentApp.swift
+//  TestAssignment
+//
+//  Created by Himanshu Sharma on 15/08/25.
+//
+
+import SwiftUI
+
+@main
+struct TestAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
